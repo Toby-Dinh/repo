311b313d39ae059d97d3d4130702511dce31db15
Toby <tobyddev@gmail.com>
2025-02-01 05:25:48 +1100

    Initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,45 +4,25 @@ import { useState } from "react";
 import Image from "next/image";
 
 export default function Home() {
-  const [position, setPosition] = useState({ x: 0, y: 0 });
-  const [message, setMessage] = useState("Will you be my valentines Alisa?");
-  const [btnVisible, setBtnVisible] = useState(true);
-
-  const moveButton = () => {
-    const randomTop = Math.floor(Math.random() * 600) - 300; // Further increased range
-    const randomLeft = Math.floor(Math.random() * 600) - 300; // Further increased range
-    setPosition({ x: randomTop, y: randomLeft });
-  };
-  const handleYesClick = () => {
-    setMessage("See you on Feb 14 :)");
-    setBtnVisible(false);
-  };
-
+  const [message, setMessage] = useState("Will you be my valentine Alisa?");
   return (
-    <div>
-      <div className="flex flex-col items-center justify-center h-screen">
-          <div className="text-4xl mb-8">
+    <div className="flex items-center justify-center h-screen">
+      {/* speech bubble */}
+      <div className="relative flex max-h-[50%] min-h-[300px] min-w-[1024px] w-[60%] mt-96">
+        <div className="relative w-[100%] flex flex-col items-center justify-stretch">
+          <div className="absolute top-0 w-[100%] h-[75%] bg-[#fdf8e3] rounded-[40%_40%_20%_20%/150%_150%_150%_150%] "></div>
+          <div className="absolute bottom-0 w-[94%] h-[40%] bg-[#fdf8e3] rounded-[5%_5%_20%_20%/100%_100%_100%_100%] "></div>
+          <div className="absolute w-full p-[1em_1em_2em_2em] text-[2.5rem] leading-[1.5em] text-[#807256] font-sans font-semibold">
             {message}
           </div>
-          <div className="mt-12 flex space-x-6 text-white text-2xl relative">
-            {btnVisible && (
-              <>
-                <button 
-                  className="p-2 px-4 rounded-md bg-blue-500 hover:bg-blue-600 z-10"
-                  onClick={handleYesClick}
-                >
-                  Yes
-                </button>
-                <button 
-                  className="p-2 px-4 rounded-md bg-red-500 hover:bg-red-600 relative transition-transform duration-300"
-                  style={{ transform: `translate(${position.x}px, ${position.y}px)` }}
-                  onMouseEnter={moveButton}
-                >
-                  No
-                </button>
-              </>
-            )}
+        </div>
+        <div className="absolute perspective-[2rem]">
+          <div className="inline-block mr-auto px-8 p-2 text-2xl text-[#FFFAE5] bg-[#81A7FF] 
+          rounded-[30%/100%_100%_120%_120%] rotate-x-[1deg] rotate-z-[-9deg] translate-x-[20%] 
+          translate-y-[-45%] font-sans font-semibold">
+            Toby Nooks
           </div>
+        </div>
       </div>
     </div>
   );
